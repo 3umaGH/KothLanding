e117ab4a2fa7f8864156a1c1e5b79f7c94eceeaa
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-04-12 15:49:02 +0300

    implemented responsive navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Footer } from './Footer'
+import { Navbar } from './Navbar'
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div
-      className='text-white font-nunito px-2 w-screen flex flex-col
-       items-center'>
+    <div className='text-white font-nunito w-screen flex flex-col items-center'>
       <div className='flex flex-col justify-between items-center min-h-screen'>
+        <Navbar />
         {children}
         <Footer />
       </div>
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from 'react'
+import LOGO from '../../assets/logo-small.webp'
+import { useBreakPoint } from '../../hooks/useBreakpoint'
+
+const links = [
+  {
+    title: 'Discord',
+    subtitle: 'Connect with the community',
+    URL: 'https://discord.kothreforged.com',
+    openInNewTab: true,
+  },
+  {
+    title: 'Donate',
+    subtitle: 'Help us to fund the servers',
+    URL: 'https://mee6.gg/m/1130814036603326464',
+    openInNewTab: false,
+  },
+  {
+    title: 'Server List',
+    subtitle: 'View our other servers',
+    URL: '#servers',
+    openInNewTab: false,
+  },
+]
+
+export const Navbar = () => {
+  const [isSideMenuOpen, setSideMenuOpen] = useState(false)
+  const currentBreakpoint = useBreakPoint()
+
+  useEffect(() => {
+    if (currentBreakpoint === 'lg') setSideMenuOpen(false)
+
+    if (isSideMenuOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [currentBreakpoint, isSideMenuOpen])
+
+  return (
+    <div
+      className={`absolute z-10 overflow-x-clip h-full w-full transition-colors duration-300 ${
+        isSideMenuOpen ? 'bg-[#121315d8]' : ''
+      }`}>
+      <div className='absolute bg-[#12131576] w-full flex justify-center min-h-[75px]'>
+        <div className='p-4 max-w-[1536px] w-full flex justify-between items-center'>
+          <img src={LOGO} width={120} className='' />
+
+          <div className='font-semibold uppercase hidden lg:flex gap-16'>
+            {links.map(link => (
+              <a
+                key={link.title}
+                href={link.URL}
+                target={link.openInNewTab ? '_blank' : undefined}
+                rel={link.openInNewTab ? 'noopener noreferrer' : undefined}
+                className='hover:opacity-80 transition-all duration-250'>
+                <p>{link.title}</p>
+                <p className='text-[0.75rem] font-thin'>{link.subtitle}</p>
+              </a>
+            ))}
+          </div>
+
+          <div className='block lg:hidden bg-red-500 z-20' onClick={() => setSideMenuOpen(p => !p)}>
+            <div className='w-5'>
+              <span
+                className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                  isSideMenuOpen ? 'rotate-45' : ''
+                }`}></span>
+              <span
+                className={`block absolute  h-0.5 w-5 bg-current   transform transition duration-500 ease-in-out ${
+                  isSideMenuOpen ? 'opacity-0' : '-translate-y-1.5'
+                }`}></span>
+              <span
+                className={`block absolute  h-0.5 w-5 bg-current transform  transition duration-500 ease-in-out ${
+                  isSideMenuOpen ? '-rotate-45' : 'translate-y-1.5'
+                }`}></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`absolute right-0 top-0 min-w-[300px] bg-[#121315dc] p-4 pr-16 pt-8 h-[100svh] transition-all duration-500 ${
+          isSideMenuOpen ? 'translate-x-0' : 'translate-x-96 invisible'
+        }`}>
+        {links.map(link => (
+          <a
+            key={link.title}
+            href={link.URL}
+            target={link.openInNewTab ? '_blank' : undefined}
+            rel={link.openInNewTab ? 'noopener noreferrer' : undefined}
+            className='hover:opacity-80 transition-all duration-250'>
+            <p>{link.title}</p>
+            <p className='text-[0.75rem] font-thin'>{link.subtitle}</p>
+          </a>
+        ))}
+      </div>
+    </div>
+  )
+}
