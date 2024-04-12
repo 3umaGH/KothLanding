ad3e3039d6b64672e3ff5c85550123b1c75a6687
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-04-12 15:54:55 +0300

    changed footer style

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,11 @@
 export const Footer = () => {
   return (
     <div className='mt-32 mb-16 w-full'>
-      <div className='bg-[#08080963] py-8 flex gap-2 flex-col justify-center items-center'>
-        <p>
-          <span className='font-bold'>King of the Hill Reforger</span> © 2024. All rights reserved.
-        </p>
-        <p>
+      <div className='bg-[#08080963] py-8 flex gap-4 flex-col justify-center items-center'>
+        <div className="flex flex-col text-center">
+        <p className='font-bold'>King of the Hill Reforger</p> <p>© 2024. All rights reserved.</p>
+        </div>
+        <p className=" text-[#7195c1]">
           Crafted with ❤️ by{' '}
           <a href='https://github.com/3umaGH' target='_blank' rel='noopener noreferrer'>
             3uma
