f6a84d577d4ab7ad8ce3853facb966618126d130
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-04-12 16:48:39 +0300

    fix alignment

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,7 +5,7 @@ import { Layout } from '../components/layout/Layout'
 
 export const Homepage = () => {
   return (
-    <div className='overflow-x-clip'>
+    <div className='overflow-x-clip flex flex-col items-center'>
       <Layout>
         <Hero />
         <hr className='w-full my-16 h-[2px] border-t-0 bg-[#1a1c1f]' />
